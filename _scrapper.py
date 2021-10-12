0267234c44c830ca65d7350d724d04d9a57a39cd
QuetzalCG <80189928+QuetzalCG@users.noreply.github.com>
2021-10-12 17:46:16 -0500

    srp2

--- _scrapper.py
+++ _scrapper.py
@@ -1,23 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+   
+def files(dato):
+    F = open("cursos.txt", "a")
+    F.write(f"{dato}\n")
+    F.close()
+
 def scrapp():
     try:
-        KEYS = ["CSS", "css", "Css", "HTML", "html", "Html", "PYTHON", "Python", "python", "JavaScript", "Javascript", "JAVASCRIPT", "PHP", "php", "Php"]
+        KEYS = ["Python", "PHP", "HTML", "CSS", "PostgreSQL", "Mysql", "JavaScript", "C#", "C++", "SQL", "Bootstrap", "MongoDB", "Angular", "Hacker", "Hacking", "Hackeo", "Linux"]
         r = requests.get("https://blog.facialix.com/cupones/")
         d = r.text
-        if "python" in d or "php" in d:
-            s = BeautifulSoup(d)
-            t = s.find_all("h2", "sek-pg-title")
-            for x in t:
-                su = x.find_all("a")
-                sub = su[0].text
-                if KEYS in sub:
-                    print(f"{sub}\n{sub.attrs.get('href')}\n\n")
-                    time.sleep(5)
-        else:
-            pass
+        s = BeautifulSoup(d, "html.parser")
+        t = s.find_all("h2", "sek-pg-title")
+        for x in t:
+            su = x.find_all("a")
+            titulo = su[0].text
+            link = su[0].attrs.get("href")
+            curso = f"\n{titulo}|{link}"
+            for y in KEYS:
+                if y in titulo:
+                    print(curso)
+                    print("Hay un curso sobre: " + y + "\n")
+                    break
+                else:
+                    pass
     except Exception as a:
-        print(a)
+        print("Hay un error: " + a)
 
 scrapp()
