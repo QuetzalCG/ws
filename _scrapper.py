6ed7c21172489ab0cc2f5384f6d57c58483d8547
QuetzalCG <80189928+QuetzalCG@users.noreply.github.com>
2021-10-12 19:51:20 -0500

    scp3

--- _scrapper.py
+++ _scrapper.py
@@ -9,24 +9,36 @@ def files(dato):
 
 def scrapp():
     try:
+        #Declaracion de llaves 
         KEYS = ["Python", "PHP", "HTML", "CSS", "PostgreSQL", "Mysql", "JavaScript", "C#", "C++", "SQL", "Bootstrap", "MongoDB", "Angular", "Hacker", "Hacking", "Hackeo", "Linux"]
         r = requests.get("https://blog.facialix.com/cupones/")
         d = r.text
         s = BeautifulSoup(d, "html.parser")
         t = s.find_all("h2", "sek-pg-title")
-        for x in t:
-            su = x.find_all("a")
+        #Iterar los datos extraidos del scrapper
+        for y in t:
+            su = y.find_all("a")
             titulo = su[0].text
             link = su[0].attrs.get("href")
             curso = f"\n{titulo}|{link}"
+            #Iterar llaves y hacer un chequeo
             for y in KEYS:
                 if y in titulo:
-                    print(curso)
-                    print("Hay un curso sobre: " + y + "\n")
+                    FILE = open("cursos.txt", "r")
+                    r = FILE.read()
+                    FILE.close()
+                    if titulo in r:
+                        pass
+                    else:
+                        files(titulo)
+                        print(f"{curso}")
                     break
                 else:
                     pass
+        time.sleep(3)
+        scrapp()
     except Exception as a:
-        print("Hay un error: " + a)
+        print(f"Hay un error: {a}")
+
 
 scrapp()
